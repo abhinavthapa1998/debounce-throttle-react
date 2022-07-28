cda0d47ce396cc72a93d87a9ab138689360ae897
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-28 22:33:42 +0530

    Final Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import "./styles.css";
+import useDebounce from "./hooks/useDebounce";
+import useThrottle from "./hooks/useThrottle";
+import { useState } from "react";
 
 export default function App() {
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
+  const throttledSearch = useThrottle(search, 1000);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Simple Debouncing and Throttling in React</h1>
+      <div style={{ border: "solid 1px gray", padding: "20px" }}>
+        <input onChange={(e) => setSearch(e.target.value)} type="text" />
+        <h2>Normal</h2>
+        <div>Normal Result: </div>
+        <div>{search}</div>
+        <h2>Debouncing</h2>
+        <div>Debounced Result: </div>
+        <div>{debouncedSearch}</div>
+        <h2>Throttling</h2>
+        <div>Throttled Result: </div>
+        <div>{throttledSearch}</div>
+      </div>
     </div>
   );
 }
--- src/hooks/useThrottle.tsx
+++ src/hooks/useThrottle.tsx
@@ -1,13 +1,26 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-function useThrottle(cb: () => void, limit: number) {
-  const lastRun = useRef(Date.now());
-  return function () {
-    if (Date.now() - lastRun.current >= limit) {
-      cb();
-      lastRun.current = Date.now();
+function useThrottle<T>(value: T, interval = 500): T {
+  const [throttledValue, setThrottledValue] = useState<T>(value);
+  const lastExecuted = useRef<number>(Date.now());
+
+  useEffect(() => {
+    //IF DIFFERENCE BETWEEN LAST CALL IS GREATER THAN INTERVAL
+    if (Date.now() >= lastExecuted.current + interval) {
+      lastExecuted.current = Date.now();
+      setThrottledValue(value);
     }
-  };
-}
+    //IF CALLED TOO EARLY BEFORE THE INTERVAL
+    else {
+      const timerId = setTimeout(() => {
+        lastExecuted.current = Date.now();
+        setThrottledValue(value);
+      }, interval);
 
+      return () => clearTimeout(timerId);
+    }
+  }, [value, interval]);
+
+  return throttledValue;
+}
 export default useThrottle;
